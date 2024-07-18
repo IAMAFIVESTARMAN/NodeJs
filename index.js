49f6e76ec43da3fb3b8232faefaec50867a6d165
Joel <michaelbluth231@gmail.com>
2024-07-18 19:16:17 +0530

    modified

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
-const fs= require("fs")
-const path=require("path")
+const fs = require("fs");
+const path = require("path");
 
-fs.readFile(path.join(__dirname,'files','starter.txt'),'utf-8',(err,dat)=>{
-    if(err) throw Error
+fs.readFile(
+  path.join(__dirname, "files", "starter.txt"),
+  "utf-8",
+  (err, dat) => {
+    if (err) throw Error;
     console.log("Read Complete");
-})
+  }
+);
 
-console.log('Hello');
+console.log("Hello");
 
-
-fs.writeFile(path.join(__dirname,'files','reply.txt'),'Nice to meet you',(err)=>{
-    if(err) throw Error
+fs.writeFile(
+  path.join(__dirname, "files", "reply.txt"),
+  "Nice to meet you",
+  (err) => {
+    if (err) throw Error;
     console.log("Write complete");
-})
+    fs.appendFile(
+      path.join(__dirname, "files", "reply.txt"),
+      "\n\nYes it is",
+      (err) => {
+        if (err) throw Error;
+        console.log("Append complete");
+        fs.rename(
+          path.join(__dirname, "files", "reply.txt"),
+          path.join(__dirname, "files", "newReply.txt"),
+          (err) => {
+            if (err) throw Error;
+            console.log("Rename complete");
+          }
+        );
+      }
+    );
+  }
+);
 
-fs.appendFile(path.join(__dirname,'files','test.txt'),'Testing the test again',(err)=>{
-    if(err) throw Error
-    console.log("Append complete");
-})
-
-process.on("uncaughtException",err=>{
-    console.log(`There was an error : ${err}`);
-    process.exit(1)
-})
+process.on("uncaughtException", (err) => {
+  console.log(`There was an error : ${err}`);
+  process.exit(1);
+});
